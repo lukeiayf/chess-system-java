1ff4d3da640d0131dfe77dd431e558981ddb1722
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-06-18 16:56:52 -0300

    adds position class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
+
     }
 }

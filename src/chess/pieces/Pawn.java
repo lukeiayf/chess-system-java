e69e9881da7e25a20c25a13a79eca4290b296ecc
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-06-20 16:17:03 -0300

    adds pawn logic

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,81 @@
+package chess.pieces;
+
+import boardGame.Board;
+import boardGame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+
+    public Pawn(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        if (getColor() == Color.WHITE) {
+            //check if the space in front is available
+            p.setValues(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsApiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //check if it is the first move and if the two spaces in front are available
+            p.setValues(position.getRow() - 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) &&
+                    !getBoard().thereIsApiece(p) &&
+                    getBoard().positionExists(p2) &&
+                    !getBoard().thereIsApiece(p2) &&
+                    getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //check northeast diagonal for opponents
+            p.setValues(position.getRow() - 1, position.getColumn() - 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //check northwest diagonal for opponents
+            p.setValues(position.getRow() - 1, position.getColumn() + 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        } else {
+            //check if the space in front is available
+            p.setValues(position.getRow() + 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsApiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //check if it is the first move and if the two spaces in front are available
+            p.setValues(position.getRow() + 2, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
+            if (getBoard().positionExists(p) &&
+                    !getBoard().thereIsApiece(p) &&
+                    getBoard().positionExists(p2) &&
+                    !getBoard().thereIsApiece(p2) &&
+                    getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //check northeast diagonal for opponents
+            p.setValues(position.getRow() + 1, position.getColumn() - 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //check northwest diagonal for opponents
+            p.setValues(position.getRow() + 1, position.getColumn() + 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+
+        return mat;
+    }
+
+    @Override
+    public String toString() {
+        return "P";
+    }
+}
